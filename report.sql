3a568e66084f48d51c8a98790ae5ffb7e9c598ca
Espen Andersen <espen.andersen@nrk.no>
2017-03-04 01:52:14 +0100

    Example SQL script.

---
+++ report.sql
@@ -0,0 +1,25 @@
+/* START SECTION */
+
+/*
+  <LATEX>
+
+    \section{MySQL database schema}
+
+    This is a small example on how to mix LaTEX formatted text and MySQL queries to build
+    a PDF research report.
+
+    We use the default MySQL database as an example.
+
+    \subsection{Table \emph{db}}
+
+    The below table describes the contants of MySQL's \emph{db} table.
+
+  </LATEX>
+  <ALIGN>Xllccc</ALIGN>
+*/
+
+USE mysql;
+
+DESCRIBE db;
+
+/* END SECTION */
